75f16611377adf863419f9534f8b2161986a8896
Diana Prusova <di.prusova@gmail.com>
2023-08-11 16:55:37 +0200

    první verze stránek

---
+++ js/index.js
@@ -0,0 +1,77 @@
+(function($){
+  $(function(){
+    // Funkce pro ovládání reakce na velikost okna
+    function checkWindowSize() {
+      var windowWidth = $(window).width();
+      var isMobileMenuOpen = $(".mobilni_menu").is(":visible");
+
+      if (windowWidth > 1025) {
+          $(".mobilni_menu").hide();
+          $(".logo").show();
+      } 
+
+      else if (windowWidth <= 390) {
+          $(".logo").hide();
+      } 
+
+      }
+
+      // Zavolání funkce při načtení stránky a při změně velikosti okna
+      checkWindowSize();
+      $(window).resize(checkWindowSize);
+
+      // Scroll menu a mobilní navigace
+      $(".jq--scroll-sluzby").click(function(){
+        $("html, body").animate({scrollTop: $(".sluzby_div").offset().top}, 600); 
+        closeMobileMenu(); 
+      });
+
+      $(".jq--scroll-kontakt").click(function(){
+        $("html, body").animate({scrollTop: $(".kontakt_div").offset().top}, 600);
+        closeMobileMenu(); 
+      });
+
+      $(".jq--scroll-reference").click(function(){
+        $("html, body").animate({scrollTop: $(".reference_div").offset().top}, 600);
+        closeMobileMenu(); 
+      });
+
+      $(".jq--scroll-metoda_tig").click(function(){
+        $("html, body").animate({scrollTop: $(".metoda_tig").offset().top}, 600); 
+      });
+
+      $(".jq--scroll-metoda_vig").click(function(){
+        $("html, body").animate({scrollTop: $(".metoda_vig").offset().top}, 600);
+      });
+
+      $(".jq--scroll-metoda_mig_mag").click(function(){
+        $("html, body").animate({scrollTop: $(".metoda_mig_mag").offset().top}, 600);
+      });
+
+      // Mobilní navigace
+      $(".jq--mobil_ikona").click(function(){
+        $(".mobilni_menu").fadeToggle();
+
+        toggleMobileIcon();
+    });
+
+    // Funkce pro zavření mobilního menu
+    function closeMobileMenu() {
+        $(".mobilni_menu").fadeOut();
+        $(".logo").fadeIn();
+        toggleMobileIcon(); 
+    }
+
+    // Funkce pro změnu obrázku ikony
+    function toggleMobileIcon() {
+        var imgSrc = $(".jq--mobil_ikona img").attr("src");
+        if (imgSrc == "img/hamburger_icon.png") {
+            $(".jq--mobil_ikona img").attr("src", "img/cross_icon.png");
+        } else {
+            $(".jq--mobil_ikona img").attr("src", "img/hamburger_icon.png");
+        }
+    }
+
+  });
+})(jQuery);
+
